04ae0d73c3eed7d686fca6b793cb2c9f3e7edd98
mariovelandiac <mariovelandiawork@gmail.com>
2023-08-02 09:20:40 -0500

    changing var for const and let

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var arr = [1, 2, 3, 4, 5];
+const arr = [1, 2, 3, 4, 5];
 
 // Función para sumar todos los elementos del array
-var sum = function(arr) {
-    var result = 0;
-    for (var i = 0; i < arr.length; i++) {
+const sum = function(arr) {
+    let result = 0;
+    for (let i = 0; i < arr.length; i++) {
         result += arr[i];
     }
     console.log('La suma es: ' + result);
@@ -12,21 +12,21 @@ var sum = function(arr) {
 sum(arr);
 
 // Función para calcular la media del array
-var average = function(arr) {
-    var result = 0;
-    for (var i = 0; i < arr.length; i++) {
+const average = function(arr) {
+    let result = 0;
+    for (let i = 0; i < arr.length; i++) {
         result += arr[i];
     }
-    var avg = result / arr.length;
+    let avg = result / arr.length;
     console.log('La media es: ' + avg);
 }
 
 average(arr);
 
 // Función para encontrar el número mayor del array
-var max = function(arr) {
-    var max = arr[0];
-    for (var i = 1; i < arr.length; i++) {
+const max = function(arr) {
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
         if (arr[i] > max) {
             max = arr[i];
         }
